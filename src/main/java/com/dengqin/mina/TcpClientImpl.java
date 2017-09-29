bace4a15d736fbf4fa18737266ed682ed0663276
DengQin <dengq55555@163.com>
2017-09-29 17:08:39 +0800

    add log

--- src/main/java/com/dengqin/mina/TcpClientImpl.java
+++ src/main/java/com/dengqin/mina/TcpClientImpl.java
@@ -17,7 +17,7 @@ import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 
 /**
- * 建立TCP通信通道，与TCP服务器通信实现
+ * mina的客户端实现，建立TCP通信通道，与TCP服务器通信实现
  * 
  * @author dq
  * 
@@ -47,16 +47,18 @@ public class TcpClientImpl implements TcpClient {
 
 		connector = new NioSocketConnector();
 		connector.setConnectTimeoutMillis(config.getConnectionTimeout());
+
+		// 注册过滤器
 		connector.getFilterChain().addLast("codec",
-				new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName(config.getEncoding()))));
+				new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName(config.getEncoding()))));// 编解码器工厂来对字符串进行编解码处理。
 		connector.getFilterChain().addLast("threadPool", new ExecutorFilter());
 		connector.getFilterChain().addLast("logger", new LoggingFilter());
 
 		// 加上，否则无法调用下面的readFuture来从session中读取到服务端返回的信息。
 		connector.getSessionConfig().setUseReadOperation(true);
 		ConnectFuture future = connector.connect(new InetSocketAddress(config.getIpAddress(), config.getPort()));
-		future.awaitUninterruptibly();
-		ioSession = future.getSession();
+		future.awaitUninterruptibly();// 等待是否连接成功，相当于是转异步执行为同步执行。
+		ioSession = future.getSession();// 连接成功后获取会话对象。如果没有上面的等待，由于connect()方法是异步的，session可能会无法获取。
 
 		return true;
 	}
@@ -72,7 +74,7 @@ public class TcpClientImpl implements TcpClient {
 		if (ioSession == null) {
 			throw new RuntimeException("会话没有开启");
 		}
-		ioSession.write(message);
+		ioSession.write(message);// 写数据，该操作是异步的
 
 		ReadFuture readFuture = null;
 		try {
@@ -84,13 +86,14 @@ public class TcpClientImpl implements TcpClient {
 	}
 
 	/**
-	 * 关闭通道
+	 * 关闭通道(仅仅是关闭了TCP的连接通道，并没有关闭Server 端、Client 端的程序)
 	 * 
 	 * @return
 	 */
 	@Override
 	public boolean close() {
-		ioSession.close(true);
+		ioSession.close(true);// 用于关闭IoSession，该操作也是异步的，参数指定true
+								// 表示立即关闭，否则就在所有的写操作都flush 之后再关闭。
 		return false;
 	}
 }
